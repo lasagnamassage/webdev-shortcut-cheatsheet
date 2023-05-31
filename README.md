f958622122bb61dec2db0acf8739188c82e9a7a2
Ashaun Thomas <ashaunthomas@siu.edu>
2023-05-30 18:38:33 -0700

    updates project

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# webdev-shortcut-cheatsheet
+# electron-system-tray-demo
 Electron app that allows users to pull up hotkeys for relevant dev tasks without disrupting workflow
+# Dependencies
+1. [Node]
+# Instructions
+1. clone this repo
+2. run `npm i`
+3. run `npm start`
+4. check your system tray :)
+
+[Node]:https://nodejs.org/en/
---
+++ src/main.js
@@ -0,0 +1,18 @@
+const electron = require('electron');
+const path = require('path');
+const { app, Tray, Menu } = electron;
+
+app.on('ready', _ => {
+    const tray = new Tray(path.join("src","icon.png"));
+    const contextMenu = Menu.buildFromTemplate([
+        { 
+            label: 'Wow',
+            click: _ => console.log('wow')
+        },
+        {
+            label:'Awesome',
+            click: _ => console.log("awesome")
+        }
+    ])
+    tray.setContextMenu(contextMenu);
+});
