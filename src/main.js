1a523fd1ac616bb598b889f68492a588d93ae741
Ashaun Thomas <ashaunthomas@siu.edu>
2023-06-07 18:44:45 -0700

    adds panel; changes trigger

--- src/main.js
+++ src/main.js
@@ -1,12 +1,28 @@
 const electron = require('electron');
 const path = require('path');
-const { app, Tray, Menu } = electron;
+const { app, Tray, Menu, globalShortcut, BrowserWindow } = electron;
+
+function createWindow () {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600
+    })
+    win.loadFile('src/panel.html')
+}
+
+  
 
 app.on('ready', _ => {
     const tray = new Tray(path.join("src","icon.png"));
+
+    globalShortcut.register('CommandOrControl+`', () => {
+        console.log('Electron loves global shortcuts!')
+        createWindow()
+    })
+
     const contextMenu = Menu.buildFromTemplate([
         { 
-            label: 'Wow',
+            label: 'Wow', 
             click: _ => console.log('wow')
         },
         {
